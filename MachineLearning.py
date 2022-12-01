dd0ecb4aa2635a052ece8748fa9bd4c14092411b
sniegbanan <michalchoczaj@icloud.com>
2022-12-01 22:40:38 +0100

    Tworzenie modelu - przygotowywanie danych, tworzenie zbiorow, trenowanie, walidacja

---
+++ MachineLearning.py
@@ -0,0 +1,173 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow import keras
+
+csv_path = "DatabaseFroML.csv"
+df = pd.read_csv(csv_path)
+
+
+titles = [
+    'Date time',
+    'Relative timestamp [s]',
+    'Node ID', 
+    'Temperature [°C]',
+    'Relative Humidity [%]', 
+    'Light Sensor 1 (Wavelength) [nm]', 
+    'Light Sensor 2 (Wavelength) [nm]',
+]
+
+feature_keys = [
+    "date_time",
+    "time (s)",
+    "int",
+    "temp (degC)",
+    "rh (%)",
+    "lumen1 (nm)",
+    "lumen2 (nm)",
+
+]
+
+date_time_key = "Date Time"
+
+split_fraction = 0.75
+train_split = int(split_fraction * int(df.shape[0]))
+step = 10
+
+past = 600
+future = 6
+learning_rate = 0.1
+batch_size = 128
+epochs = 10
+
+
+def normalize(data, train_split):
+    data_mean = data[:train_split].mean(axis=0)
+    data_std = data[:train_split].std(axis=0)
+    return (data - data_mean) / data_std
+
+print(
+    "The selected parameters are:",
+    ", ".join([titles[i] for i in [3, 4, 5, 6]]),
+)
+selected_features = [feature_keys[i] for i in [3, 4, 5, 6]]
+features = df[selected_features]
+features.index = df[date_time_key]
+features.head()
+
+features = normalize(features.values, train_split)
+features = pd.DataFrame(features)
+features.head()
+
+train_data = features.loc[0 : train_split - 1]
+val_data = features.loc[train_split:]
+
+start = past + future
+end = start + train_split
+
+x_train = train_data[[i for i in range(4)]].values
+y_train = features.iloc[start:end][[1]]
+
+sequence_length = int(past / step)
+
+dataset_train = keras.preprocessing.timeseries_dataset_from_array(
+    x_train,
+    y_train,
+    sequence_length=sequence_length,
+    sampling_rate=step,
+    batch_size=batch_size,
+)
+
+x_end = len(val_data) - past - future
+
+label_start = train_split + past + future
+
+x_val = val_data.iloc[:x_end][[i for i in range(4)]].values
+y_val = features.iloc[label_start:][[1]]
+
+dataset_val = keras.preprocessing.timeseries_dataset_from_array(
+    x_val,
+    y_val,
+    sequence_length=sequence_length,
+    sampling_rate=step,
+    batch_size=batch_size,
+)
+
+
+for batch in dataset_train.take(1):
+    inputs, targets = batch
+
+print("Input shape:", inputs.numpy().shape)
+print("Target shape:", targets.numpy().shape)
+
+inputs = keras.layers.Input(shape=(inputs.shape[1], inputs.shape[2]))
+lstm_out = keras.layers.LSTM(32)(inputs)
+outputs = keras.layers.Dense(1)(lstm_out)
+
+model = keras.Model(inputs=inputs, outputs=outputs)
+model.compile(optimizer=keras.optimizers.Adam(learning_rate=learning_rate), loss="mean_squared_error")
+model.summary()
+
+path_checkpoint = "model_checkpoint.h5"
+es_callback = keras.callbacks.EarlyStopping(monitor="val_loss", min_delta=0, patience=5)
+
+modelckpt_callback = keras.callbacks.ModelCheckpoint(
+    monitor="val_loss",
+    filepath=path_checkpoint,
+    verbose=1,
+    save_weights_only=True,
+    save_best_only=True,
+)
+
+
+history = model.fit(
+    dataset_train,
+    epochs=epochs,
+    validation_data=dataset_val,
+    callbacks=[es_callback, modelckpt_callback],
+)
+
+def visualize_loss(history, title):
+    loss = history.history["loss"]
+    val_loss = history.history["val_loss"]
+    epochs = range(len(loss))
+    plt.figure()
+    plt.plot(epochs, loss, "b", label="Training loss")
+    plt.plot(epochs, val_loss, "r", label="Validation loss")
+    plt.title(title)
+    plt.xlabel("Epochs")
+    plt.ylabel("Loss")
+    plt.legend()
+    plt.show()
+
+
+visualize_loss(history, "Training and Validation Loss")
+
+def show_plot(plot_data, delta, title):
+    labels = ["History", "True Future", "Model Prediction"]
+    marker = [".-", "rx", "go"]
+    time_steps = list(range(-(plot_data[0].shape[0]), 0))
+    if delta:
+        future = delta
+    else:
+        future = 0
+
+    plt.title(title)
+    for i, val in enumerate(plot_data):
+        if i:
+            plt.plot(future, plot_data[i], marker[i], markersize=10, label=labels[i])
+        else:
+            plt.plot(time_steps, plot_data[i].flatten(), marker[i], label=labels[i])
+    plt.legend()
+    plt.xlim([time_steps[0], (future + 5) * 2])
+    plt.xlabel("Time-Step")
+    plt.show()
+    return
+
+
+for x, y in dataset_val.take(5):
+    show_plot(
+        [x[0][:, 1].numpy(), y[0].numpy(), model.predict(x)[0]],
+        12,
+        "Single Step Prediction",
+    )
